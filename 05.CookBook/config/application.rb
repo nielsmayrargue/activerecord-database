6671d1babff396522b333419421c4baa77691bfa
jubeo <jubeo@jubeo-K43SJ.(none)>
2014-01-28 18:22:25 +0100

    solu 05

--- 05.CookBook/config/application.rb
+++ 05.CookBook/config/application.rb
@@ -1,8 +1,11 @@
+#Fichier de config de la DB. Peut aussi contenir configs portant sur d'autres éléments : gems modifiées, clés secretes d'API...
+#Fait appel au fichier yml qui spécifie certaines modalités des relations entre Active Record et la DB
+
 require 'sqlite3'
 require 'active_record'
 
-ActiveRecord::Base.configurations = YAML::load(IO.read(File.join(File.dirname(__FILE__),'database.yml')))
-ActiveRecord::Base.establish_connection('development')
+ActiveRecord::Base.configurations = YAML::load(IO.read(File.join(File.dirname(__FILE__),'database.yml'))) #dit où se trouve le fichier de conf
+ActiveRecord::Base.establish_connection('development') #utilise le fichier de conf
 
 
 # Migrations
@@ -10,8 +13,10 @@ ActiveRecord::Base.establish_connection('development')
 unless ActiveRecord::Base.connection.tables.include? "recipes"
   ActiveRecord::Schema.define do
     create_table :recipes do |t|
-      # your code here to create the schema of the Recipes table  
-       
+    	t.text :name
+    	t.text :description
+    	t.integer :length
+    	t.integer :difficulty       
     end
   end
 end
--- 05.CookBook/cookbook.rb
+++ 05.CookBook/cookbook.rb
@@ -3,20 +3,66 @@ require './models/recipe'
 
 # your program here
 
-puts "Salut Robuchon, what do you want to do today?"
-puts "1. create a recipe"
-puts "2. delete all recipes"
-puts "3. read your recipes"
+choice = ""
 
-choice = gets.chomp.to_i
+until choice == 6
 
-if choice == 1
-  
-elsif choice == 2
-  
-elsif choice == 3
-  
-else
-  puts "i did not understand"
-  
+  puts "Salut Robuchon, what do you want to do today?"
+  puts "1. create a recipe"
+  puts "2. delete all recipes"
+  puts "3. read your recipes"
+  puts "4. update a recipe"
+  puts "5. delete a recipe"
+  puts "6. exit"
+
+  choice = gets.chomp.to_i
+  puts "************"
+
+  case choice
+   when 1
+    puts "type a name"
+    name = gets.chomp
+    puts "type a description"
+    description = gets.chomp
+    puts "type a length"
+    length = gets.chomp
+    puts "type a difficulty"
+    difficulty = gets.chomp
+    Recipe.create(name: name, description: description, length: length, difficulty: difficulty)
+  when 2
+    Recipe.all.each { |recipe| recipe.destroy}
+  when 3
+    Recipe.all.each do |recipe|
+      puts "#{recipe.id}. #{recipe.name} #{recipe.description} #{recipe.length} #{recipe.difficulty}"
+    end
+  when 4
+   puts "which recipe would you like to update ? type its number"
+   Recipe.all.each do |recipe|
+      puts "#{recipe.id}. #{recipe.name} #{recipe.description} #{recipe.length} #{recipe.difficulty}"
+    end
+    id_of_the_recipe = gets.chomp.to_i
+    puts "do you want to change the name, description, length or difficulty ?"
+    element_to_change = gets.chomp
+    puts "write the new element"
+    new_element = gets.chomp
+    recipe = Recipe.find_by(id: id_of_the_recipe)
+    case element_to_change
+    when "name"
+    recipe.update(name: new_element)
+    when "desription"
+    recipe.update(description: new_element)
+    end
+  when 5
+    puts "which recipe would you like to delete ? Type its number"
+    Recipe.all.each do |recipe|
+      puts "#{recipe.id}. #{recipe.name} #{recipe.description} #{recipe.length} #{recipe.difficulty}"
+    end
+    id_of_the_recipe = gets.chomp.to_i
+    Recipe.find_by(id: id_of_the_recipe).destroy
+  when 6
+    puts "bye bye"
+  else
+    puts "i did not understand"
+    
+  end
 end
